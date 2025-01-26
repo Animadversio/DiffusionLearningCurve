b0f4fd7e5d68960461fa9140c36a561756d0d4ae
Binxu Wang <binxu_wang@hms.harvard.edu>
2025-01-26 07:49:06 -0500

    add unet learning curve

--- scripts/unet_edm_learn_curve.sh
+++ scripts/unet_edm_learn_curve.sh
@@ -10,8 +10,10 @@ python experiment/unet_learn_curve_CLI.py --exp_name MNIST_UNet_CNN_EDM_4blocks_
     --batch_size 2048 --nsteps 10000 --layers_per_block 1 --model_channels 16 --channel_mult 1 2 3 4 \
     --decoder_init_attn False --record_frequency 0  --eval_sample_size 5000
 
-python experiment/unet_learn_curve_CLI.py --exp_name MNIST_UNet_CNN_EDM_4blocks_wide64_noattn \
-    --batch_size 2048 --nsteps 10000 --layers_per_block 1 --model_channels 64 --channel_mult 1 2 3 4 \
+
+cd ~/Github/DiffusionLearningCurve
+python experiment/unet_learn_curve_CLI.py --exp_name MNIST_UNet_CNN_EDM_4blocks_noattn_denser \
+    --batch_size 2048 --nsteps 10000 --layers_per_block 1 --model_channels 16 --channel_mult 1 2 3 4 \
     --decoder_init_attn False --record_frequency 0  --eval_sample_size 5000
 
 cd ~/Github/DiffusionLearningCurve
@@ -28,4 +30,9 @@ cd ~/Github/DiffusionLearningCurve
 python experiment/unet_learn_curve_CLI.py --exp_name MNIST_UNet_CNN_EDM_2blocks_noattn \
     --batch_size 2048 --nsteps 10000 --layers_per_block 1 --model_channels 16 --channel_mult 1 2 \
     --decoder_init_attn False --record_frequency 0  --eval_sample_size 5000
+
+python experiment/unet_learn_curve_CLI.py --exp_name MNIST_UNet_CNN_EDM_4blocks_wide64_noattn \
+    --batch_size 2048 --nsteps 10000 --layers_per_block 1 --model_channels 64 --channel_mult 1 2 3 4 \
+    --decoder_init_attn False --record_frequency 0  --eval_sample_size 5000
+
 
