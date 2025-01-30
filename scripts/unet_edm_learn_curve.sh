b3bbc4c39664eaf53f719126727f5162cd91d192
Binxu Wang <binxu_wang@hms.harvard.edu>
2025-01-30 06:04:18 -0500

    addd task and analysis

--- scripts/unet_edm_learn_curve.sh
+++ scripts/unet_edm_learn_curve.sh
@@ -12,6 +12,13 @@ python experiment/unet_learn_curve_CLI.py --exp_name MNIST_UNet_CNN_EDM_4blocks_
 
 
 cd ~/Github/DiffusionLearningCurve
+python experiment/unet_learn_curve_CLI.py --exp_name MNIST_UNet_CNN_EDM_4blocks_noattn_smalllr_longtrain \
+    --batch_size 2048 --nsteps 50000 --layers_per_block 1 --model_channels 16 --channel_mult 1 2 3 4 \
+    --decoder_init_attn False --record_frequency 0  --eval_sample_size 5000 --lr 1e-4
+
+
+
+cd ~/Github/DiffusionLearningCurve
 python experiment/unet_learn_curve_CLI.py --exp_name MNIST_UNet_CNN_EDM_4blocks_noattn_denser \
     --batch_size 2048 --nsteps 10000 --layers_per_block 1 --model_channels 16 --channel_mult 1 2 3 4 \
     --decoder_init_attn False --record_frequency 0  --eval_sample_size 5000
@@ -70,6 +77,12 @@ python experiment/unet_FFHQ_learn_curve_CLI.py --dataset_name CIFAR --exp_name C
     --lr 0.001 --nsteps 50000 --batch_size 512 \
     --model_channels 128 --channel_mult 2 2 2 --attn_resolutions 16 --eval_sampling_steps 20 --layers_per_block 1
 
+
+cd ~/Github/DiffusionLearningCurve
+python experiment/unet_FFHQ_learn_curve_CLI.py --dataset_name CIFAR --exp_name CIFAR10_UNet_CNN_EDM_3blocks_wide128_attn_pilot_fixednorm_smalllr \
+    --decoder_init_attn True --record_frequency 0  --eval_sample_size 2000 --eval_batch_size 1024 \
+    --lr 1e-4 --nsteps 50000 --batch_size 512 \
+    --model_channels 128 --channel_mult 2 2 2 --attn_resolutions 16 --eval_sampling_steps 20 --layers_per_block 1
 # torchrun --standalone --nproc_per_node=8 train.py --outdir=training-runs --data=datasets/cifar10-32x32.zip --cond=1 --arch=ddpmpp
 
 
