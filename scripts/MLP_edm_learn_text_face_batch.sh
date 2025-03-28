7aa44329ac0c67926691d46760e16656b2daf083
Binxu Wang <binxu_wang@hms.harvard.edu>
2025-03-28 19:05:55 -0400

    add jobs

--- scripts/MLP_edm_learn_text_face_batch.sh
+++ scripts/MLP_edm_learn_text_face_batch.sh
@@ -4,7 +4,7 @@
 #SBATCH -c 16               # Number of cores (-c)
 #SBATCH --mem=75G           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --gres=gpu:1
-#SBATCH --array 1-3
+#SBATCH --array 4
 #SBATCH -o MLP_edm_learn_text_face_%A_%a.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e MLP_edm_learn_text_face_%A_%a.err  # File to which STDERR will be written, %j inserts jobid
 #SBATCH --mail-user=binxu_wang@hms.harvard.edu
@@ -15,6 +15,7 @@ param_list=\
 '--dataset_name  ffhq-32x32                      --exp_name FFHQ32_UNet_MLP_EDM_8L_3072D_lr1e-4                      --eval_sample_size 2048 --eval_batch_size 2048  --lr 1e-4 --nsteps 100000 --batch_size 256  --mlp_layers 8 --mlp_hidden_dim 3072 --mlp_time_embed_dim 128
 --dataset_name  ffhq-32x32-fix_words            --exp_name FFHQ32_fix_words_UNet_MLP_EDM_8L_3072D_lr1e-4             --eval_sample_size 2048 --eval_batch_size 2048  --lr 1e-4 --nsteps 100000 --batch_size 256  --mlp_layers 8 --mlp_hidden_dim 3072 --mlp_time_embed_dim 128
 --dataset_name  ffhq-32x32-random_word_jitter   --exp_name FFHQ32_random_words_jitter_UNet_MLP_EDM_8L_3072D_lr1e-4   --eval_sample_size 2048 --eval_batch_size 2048  --lr 1e-4 --nsteps 100000 --batch_size 256  --mlp_layers 8 --mlp_hidden_dim 3072 --mlp_time_embed_dim 128
+--dataset_name  CIFAR                           --exp_name CIFAR_UNet_MLP_EDM_8L_3072D_lr1e-4                        --eval_sample_size 2048 --eval_batch_size 2048  --lr 1e-4 --nsteps 100000 --batch_size 256  --mlp_layers 8 --mlp_hidden_dim 3072 --mlp_time_embed_dim 128
 '
 # --dataset_name FFHQ                      --exp_name FFHQ_UNet_CNN_EDM_4blocks_wide128_attn_pilot_fixednorm                       --decoder_init_attn True  --eval_sample_size 1000 --eval_batch_size 512  --lr 1e-4 --nsteps 50000 --batch_size 256  --model_channels 128 --channel_mult 1 2 2 2 --attn_resolutions 8 
 # --dataset_name  FFHQ_fix_words            --exp_name FFHQ_fix_words_UNet_CNN_EDM_4blocks_wide128_attn_pilot_fixednorm             --decoder_init_attn True  --eval_sample_size 1000 --eval_batch_size 512  --lr 1e-4 --nsteps 50000 --batch_size 256  --model_channels 128 --channel_mult 1 2 2 2 --attn_resolutions 8 
