9dfd7b65bc42b230852ad5df219c93579edee8cf
Binxu Wang <binxu_wang@hms.harvard.edu>
2025-04-07 19:03:01 -0400

    add new training tasks

--- scripts/unet_edm_learn_text_face_batch.sh
+++ scripts/unet_edm_learn_text_face_batch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -t 8:00:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 10:00:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p kempner_h100          # Partition to submit to
 #SBATCH -c 16               # Number of cores (-c)
 #SBATCH --mem=75G           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --gres=gpu:1
-#SBATCH --array 8
+#SBATCH --array 12-14
 #SBATCH -o unet_edm_learn_text_face_%A_%a.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e unet_edm_learn_text_face_%A_%a.err  # File to which STDERR will be written, %j inserts jobid
 #SBATCH --mail-user=binxu_wang@hms.harvard.edu
@@ -19,6 +19,12 @@ param_list=\
 --dataset_name  ffhq-32x32-random_word_jitter   --exp_name FFHQ32_random_words_jitter_UNet_CNN_EDM_4blocks_wide128_attn_pilot_fixednorm   --decoder_init_attn True  --eval_sample_size 1000 --eval_batch_size 512  --lr 1e-4 --nsteps 50000 --batch_size 256  --model_channels 128 --channel_mult 1 2 2 2 --attn_resolutions 8 
 --dataset_name  afhq-32x32                      --exp_name AFHQ32_UNet_CNN_EDM_4blocks_wide128_attn_pilot_fixednorm                       --decoder_init_attn True  --eval_sample_size 1000 --eval_batch_size 512  --lr 1e-4 --nsteps 50000 --batch_size 256  --model_channels 128 --channel_mult 1 2 2 2 --attn_resolutions 8 
 --dataset_name  CIFAR                           --exp_name CIFAR_UNet_CNN_EDM_4blocks_wide128_attn_pilot_fixednorm                        --decoder_init_attn True  --eval_sample_size 1000 --eval_batch_size 512  --lr 1e-4 --nsteps 50000 --batch_size 256  --model_channels 128 --channel_mult 1 2 2 2 --attn_resolutions 8 
+--dataset_name  ffhq-32x32                      --exp_name FFHQ32_UNet_CNN_EDM_3blocks_2x_wide128_fixednorm                       --decoder_init_attn True  --eval_sample_size 1000 --eval_batch_size 512  --lr 1e-4 --nsteps 50000 --batch_size 256  --model_channels 128 --channel_mult 1 2 2 --attn_resolutions 0  --layers_per_block 2
+--dataset_name  ffhq-32x32                      --exp_name FFHQ32_UNet_CNN_EDM_2blocks_2x_wide128_fixednorm                       --decoder_init_attn True  --eval_sample_size 1000 --eval_batch_size 512  --lr 1e-4 --nsteps 50000 --batch_size 256  --model_channels 128 --channel_mult 1 2 --attn_resolutions 0  --layers_per_block 2
+--dataset_name  ffhq-32x32                      --exp_name FFHQ32_UNet_CNN_EDM_1blocks_2x_wide128_fixednorm                       --decoder_init_attn True  --eval_sample_size 1000 --eval_batch_size 512  --lr 1e-4 --nsteps 50000 --batch_size 256  --model_channels 128 --channel_mult 1 --attn_resolutions 0    --layers_per_block 2 
+--dataset_name  afhq-32x32                      --exp_name AFHQ32_UNet_CNN_EDM_3blocks_2x_wide128_fixednorm                       --decoder_init_attn True  --eval_sample_size 1000 --eval_batch_size 512  --lr 1e-4 --nsteps 50000 --batch_size 256  --model_channels 128 --channel_mult 1 2 2 --attn_resolutions 0  --layers_per_block 2
+--dataset_name  afhq-32x32                      --exp_name AFHQ32_UNet_CNN_EDM_2blocks_2x_wide128_fixednorm                       --decoder_init_attn True  --eval_sample_size 1000 --eval_batch_size 512  --lr 1e-4 --nsteps 50000 --batch_size 256  --model_channels 128 --channel_mult 1 2 --attn_resolutions 0  --layers_per_block 2
+--dataset_name  afhq-32x32                      --exp_name AFHQ32_UNet_CNN_EDM_1blocks_2x_wide128_fixednorm                       --decoder_init_attn True  --eval_sample_size 1000 --eval_batch_size 512  --lr 1e-4 --nsteps 50000 --batch_size 256  --model_channels 128 --channel_mult 1 --attn_resolutions 0    --layers_per_block 2 
 '
 
 export param_name="$(echo "$param_list" | head -n $SLURM_ARRAY_TASK_ID | tail -1)"
