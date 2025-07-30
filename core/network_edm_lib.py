5c933593c8b9620e859564b4c722b193e552126a
Binxu Wang <binxu_wang@hms.harvard.edu>
2025-07-30 18:16:18 -0400

    add create UNet and ResNet function add resnet experiments

--- core/network_edm_lib.py
+++ core/network_edm_lib.py
@@ -5,6 +5,31 @@ import numpy as np
 import torch
 from torch.nn.functional import silu
 
+
+def create_unet_model(config):
+    unet = SongUNet(in_channels=config.channels, 
+                out_channels=config.channels, 
+                num_blocks=config.layers_per_block, 
+                attn_resolutions=config.attn_resolutions, 
+                decoder_init_attn=config.decoder_init_attn if 'decoder_init_attn' in config else True,
+                model_channels=config.model_channels, 
+                channel_mult=config.channel_mult, 
+                dropout=config.dropout, 
+                img_resolution=config.img_size, 
+                label_dim=config.label_dim,
+                embedding_type='positional', 
+                encoder_type='standard', 
+                decoder_type='standard', 
+                augment_dim=config.augment_dim, #  no augmentation , 9 for defaults. 
+                channel_mult_noise=1, 
+                resample_filter=[1,1], 
+                )
+    pytorch_total_grad_params = sum(p.numel() for p in unet.parameters() if p.requires_grad)
+    print(f'total number of trainable parameters in the Score Model: {pytorch_total_grad_params}')
+    pytorch_total_params = sum(p.numel() for p in unet.parameters())
+    print(f'total number of parameters in the Score Model: {pytorch_total_params}')
+    return unet
+
 #----------------------------------------------------------------------------
 # Unified routine for initializing weights and biases.
 
@@ -356,6 +381,116 @@ class SongUNet(torch.nn.Module):
                     x = torch.cat([x, skips.pop()], dim=1)
                 x = block(x, emb)
         return aux
+
+#----------------------------------------------------------------------------
+# Simplified UNet ResNet architecture, no skip connections, no attention, only one resolution
+class SongUNetResNet(torch.nn.Module):
+    def __init__(self,
+        img_resolution,                     # Image resolution at input/output.
+        in_channels,                        # Number of color channels at input.
+        out_channels,                       # Number of color channels at output.
+        label_dim           = 0,            # Number of class labels, 0 = unconditional.
+        augment_dim         = 0,            # Augmentation label dimensionality, 0 = no augmentation.
+
+        model_channels      = 128,          # Base multiplier for the number of channels.
+        # channel_mult        = 2,    # Per-resolution multipliers for the number of channels.
+        channel_mult_emb    = 4,            # Multiplier for the dimensionality of the embedding vector.
+        num_blocks          = 4,            # Number of residual blocks per resolution.
+        attn_resolutions    = [16],         # List of resolutions with self-attention.
+        dropout             = 0.10,         # Dropout probability of intermediate activations.
+        label_dropout       = 0,            # Dropout probability of class labels for classifier-free guidance.
+        # decoder_init_attn   = True,         # NEW ARGS, whether to add attn in the first decoder block
+        embedding_type      = 'positional', # Timestep embedding type: 'positional' for DDPM++, 'fourier' for NCSN++.
+        channel_mult_noise  = 1,            # Timestep embedding size: 1 for DDPM++, 2 for NCSN++.
+        encoder_type        = 'standard',   # Encoder architecture: 'standard' for DDPM++, 'residual' for NCSN++.
+        decoder_type        = 'standard',   # Decoder architecture: 'standard' for both DDPM++ and NCSN++.
+        resample_filter     = [1,1],        # Resampling filter: [1,1] for DDPM++, [1,3,3,1] for NCSN++.
+    ):
+        assert embedding_type in ['fourier', 'positional']
+        assert encoder_type in ['standard', 'skip', 'residual']
+        assert decoder_type in ['standard', 'skip']
+
+        super().__init__()
+        self.label_dropout = label_dropout
+        emb_channels = model_channels * channel_mult_emb
+        noise_channels = model_channels * channel_mult_noise
+        init = dict(init_mode='xavier_uniform')
+        init_zero = dict(init_mode='xavier_uniform', init_weight=1e-5)
+        init_attn = dict(init_mode='xavier_uniform', init_weight=np.sqrt(0.2))
+        block_kwargs = dict(
+            emb_channels=emb_channels, num_heads=1, dropout=dropout, skip_scale=np.sqrt(0.5), eps=1e-6,
+            resample_filter=resample_filter, resample_proj=True, adaptive_scale=False,
+            init=init, init_zero=init_zero, init_attn=init_attn,
+        )
+
+        # Mapping.
+        self.map_noise = PositionalEmbedding(num_channels=noise_channels, endpoint=True) if embedding_type == 'positional' else FourierEmbedding(num_channels=noise_channels)
+        self.map_label = Linear(in_features=label_dim, out_features=noise_channels, **init) if label_dim else None
+        self.map_augment = Linear(in_features=augment_dim, out_features=noise_channels, bias=False, **init) if augment_dim else None
+        self.map_layer0 = Linear(in_features=noise_channels, out_features=emb_channels, **init)
+        self.map_layer1 = Linear(in_features=emb_channels, out_features=emb_channels, **init)
+
+        # Encoder.
+        self.enc = torch.nn.ModuleDict()
+        cout = in_channels
+        caux = in_channels
+        # for level, mult in enumerate(channel_mult):
+        res = img_resolution
+        cin = cout
+        cout = model_channels
+        self.enc[f'{res}x{res}_conv'] = Conv2d(in_channels=cin, out_channels=cout, kernel=3, **init)
+        for idx in range(num_blocks):
+            cin = cout
+            cout = model_channels # * channel_mult
+            attn = (res in attn_resolutions)
+            self.enc[f'{res}x{res}_block{idx}'] = UNetBlock(in_channels=cin, out_channels=cout, attention=attn, **block_kwargs)
+
+        # Decoder.
+        self.dec = torch.nn.ModuleDict()
+        self.dec[f'{res}x{res}_aux_norm'] = GroupNorm(num_channels=cout, eps=1e-6)
+        self.dec[f'{res}x{res}_aux_conv'] = Conv2d(in_channels=cout, out_channels=out_channels, kernel=3, **init_zero)
+
+    def forward(self, x, noise_labels, cond, augment_labels=None):
+        # Mapping.
+        emb = self.map_noise(noise_labels)
+        emb = emb.reshape(emb.shape[0], 2, -1).flip(1).reshape(*emb.shape) # swap sin/cos
+        if self.map_label is not None:
+            tmp = cond
+            if self.training and self.label_dropout:
+                tmp = tmp * (torch.rand([x.shape[0], 1], device=x.device) >= self.label_dropout).to(tmp.dtype)
+            emb = emb + self.map_label(tmp * np.sqrt(self.map_label.in_features))
+        if self.map_augment is not None and augment_labels is not None:
+            emb = emb + self.map_augment(augment_labels)
+        emb = silu(self.map_layer0(emb))
+        emb = silu(self.map_layer1(emb))
+
+        # Encoder.
+        aux = x
+        for name, block in self.enc.items():
+            if 'aux_down' in name:
+                aux = block(aux)
+            elif 'aux_skip' in name:
+                x = x + block(aux)
+            elif 'aux_residual' in name:
+                x = aux = (x + block(aux)) / np.sqrt(2)
+            else:
+                x = block(x, emb) if isinstance(block, UNetBlock) else block(x)
+
+        # Decoder.
+        aux = None
+        tmp = None
+        for name, block in self.dec.items():
+            if 'aux_up' in name:
+                aux = block(aux)
+            elif 'aux_norm' in name:
+                tmp = block(x)
+            elif 'aux_conv' in name:
+                tmp = block(silu(tmp))
+                aux = tmp if aux is None else tmp + aux
+            else:
+                x = block(x, emb)
+        return aux
+
 
 #----------------------------------------------------------------------------
 # Reimplementation of the ADM architecture from the paper
